f6af599457c1edddc65c6ebd1e396552d2e5681c
eva astudillo <eva.astudillox@gmail.com>
2018-05-31 16:31:58 -0400

    cambio de cajas

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,39 @@
-window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
+window.cipher =({
+  encode: (offset,str) => {
+  
+    str=document.getElementById("encriptarTexto").value;
+      offset=document.getElementById("offset").value;
+       /* Aqui se guardara el texto ya codificado con el numero de desplazamientos*/
+         for(let i = 0; i < str.length; i++) {
+            //traducira al número de la letra en el Código ASCII
+            if(65 <= str.charCodeAt(i) && str.charCodeAt(i) <= 90){
+                //Condicion para las mayusculas en ASCII
+          let msgEncode = (((str.charCodeAt(i) - 65 + parseInt(offset)) % 26) + 65);
+          encodeResult += String.fromCharCode (msgEncode);
+             //Agregando la nueva posicion y pasandolo a letra
+            } else { 
+               //En el caso de aparecer espacios 
+        encodeResult += str.charAt(i)}; //por que es text
   }
-}
+  	     return encodeResult;
+  },
+
+    decode: (offset,srt) => {
+      srt=document.getElementById("desencriptarTexto").value;
+      offset=document.getElementById("offset").value;
+       /* Aqui se guardara el texto ya codificado con el numero de desplazamientos*/
+         for(let i = 0; i < str.length; i++) {
+            //traducira al número de la letra en el Código ASCII
+            if(65 <= str.charCodeAt(i) && str.charCodeAt(i) <= 90){
+                //Condicion para las mayusculas en ASCII
+              let msgEncode = (((str.charCodeAt(i) + 65 - parseInt(offset)) % 26) + 65);
+              decodeResult += String.fromCharCode (msgEncode);
+             //Agregando la nueva posicion y pasandolo a letra
+            } else { 
+               //En el caso de aparecer espacios 
+            decodeResult += str.charAt(i)}; //por que es texto
+          }
+             return decodeResult;
+
+     }
+});
