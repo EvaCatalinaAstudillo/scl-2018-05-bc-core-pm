495eb6bc468bf6c85883e42c70a39d11902f9bc7
eva astudillo <eva.astudillox@gmail.com>
2018-06-01 15:34:19 -0400

    proyectont

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-window.onload = function introducirNombre() {
-    document.getElementById("name").innerHTML = " Bienvenid@ " + document.getElementById("user_name").value;
+//window.onload = 
+//function introducirNombre() {
+ // document.getElementById("user_name").innerHTML = " Bienvenid@ " + document.getElementById("user_name").value;
+
+    
+ window.onload = function introducirNombre() {
+  document.getElementById("user_name").innerHTML = " Bienvenid@ " + document.getElementById("user_name").value;
   }
-  
+
+document.getElementById("btnOne").addEventListener("click",()=>{
+  string = document.getElementById("encriptarTexto").value;
+  offset = document.getElementById("offset").value;
+ document.getElementById("caja1").innerHTML = cipher.encode(string,offset);
+
+});
+
+document.getElementById("btnTwo").addEventListener("click",()=>{
+  string = document.getElementById("encriptarTexto").value;
+  offset = document.getElementById("offset").value;
+ document.getElementById("caja1").innerHTML = cipher.encode(string,offset);
+
+});
