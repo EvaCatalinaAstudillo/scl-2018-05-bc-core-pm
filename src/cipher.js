6060bde4b2fe9a02fb91573bf52f772e505bf4db
eva astudillo <eva.astudillox@gmail.com>
2018-06-20 00:26:34 -0400

    arreglos cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,45 +1,25 @@
-window.cipher =({
-  encode: (string,offset) => {
-
-    string = document.getElementById("encriptarTexto").value;
-    offset = document.getElementById("offset").value;
-
-    let encodeResult = "";
-
-    /* Aqui se guardara el texto ya codificado con el numero de desplazamientos*/
-     for(let i = 0; i < string.length; i++) {
-        //traducira al número de la letra en el Código ASCII
-  	    if(65 <= string.charCodeAt(i) && string.charCodeAt(i) <= 90){
-  	        //Condicion para las mayusculas en ASCII
-          let msgEncode = (((string.charCodeAt(i) - 65 + parseInt(offset)) % 26) + 65);
-          encodeResult += String.fromCharCode(msgEncode);
-  	     //Agregando la nueva posicion y pasandolo a letra
-  	    } else { 
-  	       //En el caso de aparecer espacios 
-        encodeResult += string.charAt(i)}; //por que es text
-  	  }
-         return encodeResult;
-
+window.cipher = {
+  encode:  (string, offset)=> {
+    var mensajeCifrado = " ";
+    for (index = 0; index < string.length; index++) {
+      string = string.toUpperCase();
+      var getIndexACSII = string.charCodeAt(index);
+      var formulaACSII = (getIndexACSII - 65 + offset) % 26 + 65;
+      var cipherText = String.fromCharCode(formulaACSII);
+      mensajeCifrado += cipherText;
+    }
+    return mensajeCifrado;
   },
-  
-    decode: (string,offset) => {
-      string = document.getElementById("encriptarTexto").value;
-      offset = document.getElementById("offset").value;
-      
-       /* Aqui se guardara el texto ya codificado con el numero de desplazamientos*/
-         for(let i = 0; i < string.length; i++) {
-            //traducira al número de la letra en el Código ASCII
-            if(65 <= string.charCodeAt(i) && string.charCodeAt(i) <= 90){
-                //Condicion para las mayusculas en ASCII
-              let msgEncode = (((string.charCodeAt(i) + 65 - parseInt(offset)) % 26) + 65);
-              decodeResult += String.fromCharCode (msgEncode);
-             //Agregando la nueva posicion y pasandolo a letra
-            } else { 
-               //En el caso de aparecer espacios 
-            decodeResult += string.charAt(i)}; //por que es texto
-          }
-             return decodeResult;
-             
 
-     }
-});
+  decode: (string, offset) => {
+    var mensajeCifrado = " ";
+    for (index = 0; index < string.length; index++) {
+      string = string.toUpperCase();
+      var getIndexACSII = string.charCodeAt(index);
+      var formulaACSII = (getIndexACSII + 65 - offset) % 26 + 65;
+      var cipherText = String.fromCharCode(formulaACSII);
+      mensajeCifrado += cipherText;
+    }
+    return mensajeCifrado;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
-//window.onload = 
-//function introducirNombre() {
- // document.getElementById("user_name").innerHTML = " Bienvenid@ " + document.getElementById("user_name").value;
-
-    
- window.onload = function introducirNombre() {
-  document.getElementById("user_name").innerHTML = " Bienvenid@ " + document.getElementById("user_name").value;
+function Info1(){
+  var offset = parseInt(document.getElementById("ingresanumero").value);
+  var string = document.getElementById("escribirmensaje").value;
+  document.getElementById("mensajeCifrado").innerText= window.cipher.encode(string, offset);
+  }
+  function Info2(){
+  var offset = parseInt(document.getElementById("ingresanumero").value);
+  var string = document.getElementById("escribirmensaje").value;
+  document.getElementById("mensajedescifrado").innerText= window.cipher.decode(string, offset);
   }
 
-document.getElementById("btnOne").addEventListener("click",()=>{
-  string = document.getElementById("encriptarTexto").value;
-  offset = document.getElementById("offset").value;
- document.getElementById("caja1").innerHTML = cipher.encode(string,offset);
 
-});
-
-document.getElementById("btnTwo").addEventListener("click",()=>{
-  string = document.getElementById("encriptarTexto").value;
-  offset = document.getElementById("offset").value;
- document.getElementById("caja1").innerHTML = cipher.encode(string,offset);
-
-});
+  var Limpiar = document.getElementById("Info1","Info2").value; 
